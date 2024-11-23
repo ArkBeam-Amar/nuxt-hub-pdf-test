2eb4182d40c404ba04051180e8f2f2332900e6d0
Amar Gill <amar.s.gill@outlook.com>
2024-11-23 15:36:36 -0500

    render pdf

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
 const runtimeConfig = useRuntimeConfig()
-const colors = ['#f87171', '#fb923c', '#fbbf24', '#facc15', '#a3e635', '#4ade80', '#34d399', '#2dd4bf', '#22d3ee', '#38bdf8', '#60a5fa', '#818cf8', '#a78bfa', '#c084fc', '#e879f9', '#f472b6', '#fb7185']
-const color = useState('color', () => colors[Math.floor(Math.random() * colors.length)])
+const colors = [
+  '#f87171',
+  '#fb923c',
+  '#fbbf24',
+  '#facc15',
+  '#a3e635',
+  '#4ade80',
+  '#34d399',
+  '#2dd4bf',
+  '#22d3ee',
+  '#38bdf8',
+  '#60a5fa',
+  '#818cf8',
+  '#a78bfa',
+  '#c084fc',
+  '#e879f9',
+  '#f472b6',
+  '#fb7185',
+]
+const color = useState(
+  'color',
+  () => colors[Math.floor(Math.random() * colors.length)],
+)
 </script>
 
 <template>
@@ -12,6 +33,9 @@ const color = useState('color', () => colors[Math.floor(Math.random() * colors.l
     <NuxtLink to="/" external>
       refresh
     </NuxtLink>
+    <div>
+      <a href="/render.pdf" target="_blank">Render PDF</a>
+    </div>
   </div>
 </template>
 
@@ -24,7 +48,18 @@ const color = useState('color', () => colors[Math.floor(Math.random() * colors.l
   left: 50%;
   transform: translate(-50%, -50%);
   margin: 0;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family:
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    "Segoe UI",
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    "Open Sans",
+    "Helvetica Neue",
+    sans-serif;
 }
 h1 {
   font-size: 32px;
